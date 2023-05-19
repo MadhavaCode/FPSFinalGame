c0cfaf07ccdb19ee4bba52c02610a18f2c8ceec1
EDHScode <kcluchey@ugcloud.ca>
2023-05-19 10:14:55 -0400

    InteractableAIConfigInit

--- FinalSinglePlayerFPSGame/Assets/Scripts/Interactable.cs
+++ FinalSinglePlayerFPSGame/Assets/Scripts/Interactable.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public abstract class Interactable : MonoBehaviour
 {
-
+    public bool useEvents;
+    [SerializeField]
     public string promptMessage;
 
     public void BaseInteract()
     {
+        if(useEvents)
+            GetComponent<InteractionEvent>().OnInteract.Invoke();
         Interact();
     }
 
